2baca133d7dedff08be8f55cb450c2bd133d20d4
zhaowenpeng <484235492@qq.com>
2024-11-08 21:04:35 +0800

    feat: 任务执行一次完成

--- vue3-cc-job-admin/src/views/task/task-info/operation/edit-task-info.vue
+++ vue3-cc-job-admin/src/views/task/task-info/operation/edit-task-info.vue
@@ -3,218 +3,180 @@
     <el-dialog
       v-model="props.editTaskInfoVal"
       title="更新任务"
-      width="650"
+      width="850px"
       :before-close="handleClose"
     >
+      <div class="info_form">
+        <div class="child_form">
+          <div style="color: #8e8e8e; font-size: 14px">基础配置</div>
+          <el-divider style="margin: 8px" />
+          <div class="child_main">
+            <div class="m_left">
+              <div class="c_cont">
+                <span>执行器*</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="c_cont">
+                <span>负责人*</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+            </div>
+            <div class="m_right">
+              <div class="c_cont">
+                <span>任务描述*</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="c_cont">
+                <span>报警邮件</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="child_form">
+          <div style="color: #8e8e8e; font-size: 14px">调度配置</div>
+          <el-divider style="margin: 8px" />
+          <div class="child_main">
+            <div class="m_left">
+              <div class="c_cont">
+                <span>调度类型*</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+            </div>
+            <div class="m_right">
+              <div class="c_cont">
+                <span>CRON*</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="child_form">
+          <div style="color: #8e8e8e; font-size: 14px">任务配置</div>
+          <el-divider style="margin: 8px" />
+          <div class="child_main">
+            <div class="m_left">
+              <div class="c_cont">
+                <span>运行模式</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+            </div>
+            <div class="m_right">
+              <div class="c_cont">
+                <span>JobHandler*</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="child_form">
+          <div class="child_main">
+            <div class="m_left">
+              <div class="c_cont_param">
+                <span>任务参数*</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="child_form">
+          <div style="color: #8e8e8e; font-size: 14px">高级配置</div>
+          <el-divider style="margin: 8px" />
+          <div class="child_main">
+            <div class="m_left">
+              <div class="c_cont">
+                <span>路由策略*</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="c_cont">
+                <span>调度过期策略</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="c_cont">
+                <span>任务超时时间</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+            </div>
+            <div class="m_right">
+              <div class="c_cont">
+                <span>子任务id</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="c_cont">
+                <span>阻塞处理策略</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="c_cont">
+                <span>失败重试次数</span>
+                <el-input
+                  v-model="taskInfoForm.taskName"
+                  type="text"
+                  autocomplete="off"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <template #footer>
-        <el-form :model="taskInfoForm" class="demo-ruleForm">
-          <!-- 基础配置 -->
-          <el-row>
-            <el-row style="width: 100%">
-              <span style="color: #8e8e8e; font-size: 14px">基础配置</span>
-              <el-divider style="margin: 8px" />
-            </el-row>
-            <el-row justify="space-around" style="width: 100%">
-              <el-col :span="12">
-                <el-form-item label="执行器" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="任务描述" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-            </el-row>
-            <el-row justify="space-around" style="width: 100%">
-              <el-col :span="12">
-                <el-form-item label="执行器" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="任务描述" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-            </el-row>
-          </el-row>
-          <!-- ----------- -->
-
-          <!-- 调度配置 -->
-          <el-row>
-            <el-row style="width: 100%">
-              <span style="color: #8e8e8e; font-size: 14px">调度配置</span>
-              <el-divider style="margin: 8px" />
-            </el-row>
-            <el-row justify="space-around" style="width: 100%">
-              <el-col :span="12">
-                <el-form-item label="调度类型" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="Cron" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-            </el-row>
-          </el-row>
-          <!-- ----------- -->
-
-          <!-- 任务配置 -->
-          <el-row>
-            <el-row style="width: 100%">
-              <span style="color: #8e8e8e; font-size: 14px">任务配置</span>
-              <el-divider style="margin: 8px" />
-            </el-row>
-            <el-row justify="space-around" style="width: 100%">
-              <el-col :span="12">
-                <el-form-item label="运行模式" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="JobHandler" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-            </el-row>
-            <el-row justify="space-around" style="width: 100%">
-              <el-col :span="24">
-                <el-form-item label="任务参数" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-            </el-row>
-          </el-row>
-          <!-- ----------- -->
-
-          <!-- 高级配置 -->
-          <el-row>
-            <el-row style="width: 100%">
-              <span style="color: #8e8e8e; font-size: 14px">高级配置</span>
-              <el-divider style="margin: 8px" />
-            </el-row>
-            <el-row style="width: 100%">
-              <el-col :span="12">
-                <el-form-item label="路由策略" prop="pass">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                    style="width: 200px; margin-left: 28px"
-                  />
-                </el-form-item>
-                <el-form-item label="调度过期策略" prop="pass">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                    style="width: 200px"
-                  />
-                </el-form-item>
-                <el-form-item label="调度过期策略" prop="pass">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                    style="width: 200px"
-                  />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="子任务id" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-            </el-row>
-            <!-- <el-row justify="space-around" style="width: 100%">
-              <el-col :span="12">
-                <el-form-item label="调度过期策略" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="阻塞处理策略" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-            </el-row>
-            <el-row justify="space-around" style="width: 100%">
-              <el-col :span="12">
-                <el-form-item label="任务超时时间" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="失败重试次数" prop="pass" width="200px">
-                  <el-input
-                    v-model="taskInfoForm.executorParam"
-                    type="text"
-                    autocomplete="off"
-                  />
-                </el-form-item>
-              </el-col>
-            </el-row> -->
-          </el-row>
-          <el-form-item>
-            <el-button type="primary" @click="submitForm()"> 保存 </el-button>
-            <el-button @click="resetForm()">取消</el-button>
-          </el-form-item>
-        </el-form>
+        <el-button type="primary" @click="submitForm()"> 保存 </el-button>
+        <el-button @click="resetForm()">取消</el-button>
       </template>
     </el-dialog>
   </div>
@@ -246,3 +208,44 @@ const resetForm = () => {
   emit("close");
 };
 </script>
+<style lang="scss" scoped>
+.info_form {
+  width: 100%;
+  .child_form {
+    width: 100%;
+    .child_main {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin: 0 50px;
+
+      .c_cont {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 10px;
+        span {
+          display: inline-block;
+          width: 140px;
+          font-size: 14px;
+          text-align: right;
+          margin-right: 10px;
+        }
+      }
+      .c_cont_param {
+        display: flex;
+        align-items: center;
+        width: 710px;
+        margin-left: 10px;
+        span {
+          display: inline-block;
+          width: 100px;
+          font-size: 14px;
+          text-align: right;
+          margin-right: 10px;
+        }
+      }
+    }
+  }
+}
+</style>
