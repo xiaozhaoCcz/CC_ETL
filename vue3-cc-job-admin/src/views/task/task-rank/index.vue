7333a5695d026e6957fcd39501dcfd504aafb54e
zhaowenpeng <484235492@qq.com>
2024-11-20 22:30:45 +0800

    feat: 任务集运行...

--- vue3-cc-job-admin/src/views/task/task-rank/index.vue
+++ vue3-cc-job-admin/src/views/task/task-rank/index.vue
@@ -3,30 +3,30 @@
     <div class="task_rank_top">
       <div class="btn_left_list">
         <el-button
-          type="primary"
           v-if="!showTaskVisible"
+          type="primary"
           @click="showTaskVisible = true"
         >
           展示任务
         </el-button>
-        <el-button type="info" v-else @click="showTaskVisible = false">
+        <el-button v-else type="info" @click="showTaskVisible = false">
           隐藏任务
         </el-button>
       </div>
       <div class="btn_right_list">
         <el-button
+          v-if="triggerOneVisible"
           type="warning"
           :icon="Loading"
           circle
           @click="stopTrigger"
-          v-if="triggerOneVisible"
         />
         <el-button
+          v-else
           type="success"
           :icon="ArrowRight"
           circle
           @click="triggerOne"
-          v-else
         />
         <el-button
           type="info"
@@ -50,7 +50,7 @@
           @node-click="selectTaskSetNode"
         />
       </div>
-      <div class="task_info_tree" v-if="showTaskVisible">
+      <div v-if="showTaskVisible" class="task_info_tree">
         <el-input v-model="filterTaskInfoText" placeholder="Filter keyword" />
 
         <el-tree
@@ -67,10 +67,10 @@
             </span>
             <div style="margin-left: 5px">
               <el-tag
+                v-if="data.jobType == 2"
                 type="primary"
                 round
                 size="small"
-                v-if="data.jobType == 2"
               >
                 任务组
               </el-tag>
@@ -86,7 +86,10 @@
           @edge-update="onEdgeUpdate"
           @edge-update-start="onEdgeUpdateStart"
           @edge-update-end="onEdgeUpdateEnd"
-        ></VueFlow>
+        >
+          <MiniMap />
+          <Background />
+        </VueFlow>
       </div>
     </div>
 
@@ -94,14 +97,14 @@
       :taskRankVisible="taskRankVisible"
       :formData="formData"
       @close="handleCloseDialog"
-    ></EditTaskRank>
+    />
 
     <EditTaskNode
       :taskNodeVisible="taskNodeVisible"
       :nodeTaskId="nodeTaskId"
       :nowDate="nowDate"
       @close="closeDraw"
-    ></EditTaskNode>
+    />
   </div>
 </template>
 <script setup lang="ts">
@@ -111,6 +114,8 @@ import "@vue-flow/core/dist/style.css";
 import "@vue-flow/core/dist/theme-default.css";
 import { ref, onMounted } from "vue";
 import { VueFlow, useVueFlow, MarkerType } from "@vue-flow/core";
+import { Background } from "@vue-flow/background";
+import { MiniMap } from "@vue-flow/minimap";
 import { Folder, ArrowRight, Loading } from "@element-plus/icons-vue";
 import TaskInfoAPI, { TaskInfoForm } from "@/api/task/task-info";
 const {
