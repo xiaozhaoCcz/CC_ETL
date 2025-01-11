a2eacc25214caf9dac3862319f63214931f0896b
zhaowenpeng <484235492@qq.com>
2025-01-11 12:58:08 +0800

    feta：前端功能完善

--- vue3-cc-job-admin/src/views/task/job-platform/index.vue
+++ vue3-cc-job-admin/src/views/task/job-platform/index.vue
@@ -99,27 +99,10 @@ const lf = ref(null);
 const lfRef = ref(null);
 const patternItems = [
   {
-    type: "dynamic-group",
-    label: "内置动态分组",
-    text: "DynamicGroup",
-    icon: "https://cdn.jsdelivr.net/gh/Logic-Flow/static@latest/docs/examples/extension/group/group.png",
-    properties: {
-      isCollapsed: false,
-      isRestrict: true,
-      autoResize: true,
-    },
-  },
-  {
     type: "circle",
-    label: "圆形",
+    label: "添加任务",
     text: "Circle",
-    icon: "https://cdn.jsdelivr.net/gh/Logic-Flow/static@latest/docs/examples/extension/group/circle.png",
-  },
-  {
-    type: "rect",
-    label: "矩形",
-    text: "Rect",
-    icon: "https://cdn.jsdelivr.net/gh/Logic-Flow/static@latest/docs/examples/extension/group/rect.png",
+    icon: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAAH6ji2bAAAABGdBTUEAALGPC/xhBQAAAnBJREFUOBGdVL1rU1EcPfdGBddmaZLiEhdx1MHZQXApraCzQ7GKLgoRBxMfcRELuihWKcXFRcEWF8HBf0DdDCKYRZpnl7p0svLe9Zzbd29eQhTbC8nv+9zf130AT63jvooOGS8Vf9Nt5zxba7sXQwODfkWpkbjTQfCGUd9gIp3uuPP8bZ946g56dYQvnBg+b1HB8VIQmMFrazKcKSvFW2dQTxJnJdQ77urmXWOMBCmXM2Rke4S7UAW+/8ywwFoewmBps2tu7mbTdp8VMOkIRAkKfrVawalJTtIliclFbaOBqa0M2xImHeVIfd/nKAfVq/LGnPss5Kh00VEdSzfwnBXPUpmykNss4lUI9C1ga+8PNrBD5YeqRY2Zz8PhjooIbfJXjowvQJBqkmEkVnktWhwu2SM7SMx7Cj0N9IC0oQXRo8xwAGzQms+xrB/nNSUWVveI48ayrFGyC2+E2C+aWrZHXvOuz+CiV6iycWe1Rd1Q6+QUG07nb5SbPrL4426d+9E1axKjY3AoRrlEeSQo2Eu0T6BWAAr6COhTcWjRaYfKG5csnvytvUr/WY4rrPMB53Uo7jZRjXaG6/CFfNMaXEu75nG47X+oepU7PKJvvzGDY1YLSKHJrK7vFUwXKkaxwhCW3u+sDFMVrIju54RYYbFKpALZAo7sB6wcKyyrd+aBMryMT2gPyD6GsQoRFkGHr14TthZni9ck0z+Pnmee460mHXbRAypKNy3nuMdrWgVKj8YVV8E7PSzp1BZ9SJnJAsXdryw/h5ctboUVi4AFiCd+lQaYMw5z3LGTBKjLQOeUF35k89f58Vv/tGh+l+PE/wG0rgfIUbZK5AAAAABJRU5ErkJggg==",
   },
   {
     label: "选区",
@@ -258,7 +241,6 @@ async function confirmDialog() {
 
   const graphModel = lf.value.graphModel;
   if (_jobInfo.jobType === 2) {
-
     //新增任务组
     let data = {} as any;
     await JobInfoAPI.getJobCompose(jobSelectId.value).then(
@@ -277,19 +259,35 @@ async function confirmDialog() {
         JSON.parse(n.children).forEach((id: any) => node.addChild(id));
       }
     });
-    const oNodes = lf.value!.getGraphRawData().nodes;
-    lf.value.graphModel.clearData();
-    oNodes.forEach((n: any) => {
-      graphModel.addNode(n);
-    });
+
     newEdges.forEach((e: any) => {
       graphModel.addEdge(generateEdge(e));
     });
-    // setTimeout(() => {
-    //   lf.value.graphModel.moveNodes([jobNode.id], 50, 50);
-    // }, 2000);
+
+    const oNodes = lf.value!.getGraphRawData().nodes;
+    const oEdges = lf.value!.getGraphRawData().edges;
+    //删除之前的节点
+    const delNodes = lf.value!.getGraphRawData().nodes;
+    const delEdges = lf.value!.getGraphRawData().edges;
+
+    delNodes.forEach((n: any) => {
+      graphModel.deleteNode(n.id);
+    });
+    delEdges.forEach((e: any) => {
+      graphModel.deleteEdgeById(e.id);
+    });
+
+    oNodes.forEach((n: any) => {
+      graphModel.addNode(n);
+    });
+    oEdges.forEach((e: any) => {
+      graphModel.addEdge(e);
+    });
+
+    graphModel.moveNodes([jobNode.id], _node.x - 500, _node.y - 230);
+
     graphModel.deleteNode(_node.id);
-  }else{
+  } else {
     _node.setProperty("jobId", jobSelectId.value);
     _node.updateText(_jobInfo.jobDesc);
   }
