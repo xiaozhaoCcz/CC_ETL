4dea51c27c69ef439ce00dfd93b8bcff77bf03a2
zhaowenpeng <484235492@qq.com>
2025-01-12 15:19:20 +0800

    feta：任务组功能优化

--- vue3-cc-job-admin/src/views/task/job-platform/index.vue
+++ vue3-cc-job-admin/src/views/task/job-platform/index.vue
@@ -583,6 +583,7 @@ const reconnectAttempts = ref(0);
 const maxReconnectAttempts = 3; // 自定义最大重试次数
 
 const connectWs = (id: string) => {
+  // TODO 后端做多节点部署时，需要修改
   ws.value = new WebSocket("ws://localhost:8989/ws/" + id);
   ws.value.onopen = () => {
     reconnectAttempts.value = 0;
