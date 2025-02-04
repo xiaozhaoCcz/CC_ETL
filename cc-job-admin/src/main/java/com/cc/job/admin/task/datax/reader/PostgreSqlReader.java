48ef1776949c81e8265275d1cfc6f85e78cf00e8
hui <1667159785@qq.com>
2025-02-04 18:06:34 +0800

    feat: 增加PostgreSqlReader(Unfinished)

---
+++ cc-job-admin/src/main/java/com/cc/job/admin/task/datax/reader/PostgreSqlReader.java
@@ -0,0 +1,19 @@
+package com.cc.job.admin.task.datax.reader;
+
+import cn.hutool.json.JSONObject;
+import com.cc.job.admin.task.datax.BaseRW;
+import com.cc.job.xo.model.datax.DataXParams;
+
+/**
+ * @author astro
+ * @description: TODO
+ * @date 2025/2/1 16:34
+ */
+public class PostgreSqlReader implements BaseRW {
+
+    @Override
+    public JSONObject buildJson(DataXParams dataXParams) {
+        JSONObject readerConfig = new JSONObject();
+        return null;
+    }
+}
